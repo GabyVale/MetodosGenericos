478387b8dd5635d8bfc219b3100391b476543fc7
GabyVale <48566458+GabyVale@users.noreply.github.com>
2019-03-21 18:07:30 -0600

    Add files via upload

---
+++ Genericos/Genericos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculadora<int> calcu = new calculadora<int>();
+            Console.WriteLine(calculadora<int>.sum(3,2));
+            Console.ReadKey();
+        }
+    }
+        
+    }
+
---
+++ Genericos/Genericos/calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    public class calculadora <T> where T : struct // Asi indicamos que es una clase generica, ayuda a maximizar la reutilización de código
+    {
+        public T _a, _b;
+        public calculadora(T i, T j)
+        {
+            _a = i;
+            _b = j;
+        }
+        public calculadora()
+        { }
+        public static calculadora<T> operator +(calculadora<T> i, calculadora<T> j)
+        {
+            Console.WriteLine("Suma: ",sum(i._a, j._a), sum(i._b, j._b));
+            return new calculadora<T>(sum(i._a, j._a), sum(i._b, j._b));
+        }
+        public static T sum(T a, T b)
+        {
+            return (dynamic)a + (dynamic)b;
+        }
+
+    }
+}
